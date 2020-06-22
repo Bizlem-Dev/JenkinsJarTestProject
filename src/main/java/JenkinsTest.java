a42b2c994a08c7809ca5d0da1b818b003a52766c
Bizlem-Dev <abhishek.tayshete@bizlem.com>
2020-06-22 19:36:34 +0530

    first apps

---
+++ src/main/java/JenkinsTest.java
@@ -0,0 +1,9 @@
+
+public class JenkinsTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("welcome to test sertver through jenkins ssh publish plugin");
+	}
+
+}
